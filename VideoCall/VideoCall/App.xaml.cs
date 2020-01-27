50748c7dc19561186d5b5f4c65af7cf4bd0162e2
Hallison Bittencourt <hallisonmkb@gmail.com>
2020-01-27 00:05:20 +0000

    Add project files.

---
+++ VideoCall/VideoCall/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using VideoCall.Services;
+using VideoCall.Views;
+using Xamarin.Forms.OpenTok.Service;
+
+namespace VideoCall
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            CrossOpenTok.Current.ApiKey = "46499332";
+            CrossOpenTok.Current.SessionId = "2_MX40NjQ5OTMzMn5-MTU3OTk5NjMxNTg5M35hWVpMQ2FtZGFqQTlNUHgwTm9heE5HZmV-fg";
+            CrossOpenTok.Current.UserToken = "T1==cGFydG5lcl9pZD00NjQ5OTMzMiZzaWc9ZjAxZTVlMWEzM2QwMGEzY2RkMzM5M2I1NjQyZWI4Njc4M2IyMWYxZjpzZXNzaW9uX2lkPTJfTVg0ME5qUTVPVE16TW41LU1UVTNPVGs1TmpNeE5UZzVNMzVoV1ZwTVEyRnRaR0ZxUVRsTlVIZ3dUbTloZUU1SFptVi1mZyZjcmVhdGVfdGltZT0xNTc5OTk2MzI2Jm5vbmNlPTAuMDU5OTQ5ODc0MzU1Njc1ODkmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTU4MjU4ODMyNiZpbml0aWFsX2xheW91dF9jbGFzc19saXN0PQ==";
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ VideoCall/VideoCall/Views/ItemsPage.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using VideoCall.Models;
+using VideoCall.Views;
+using VideoCall.ViewModels;
+using Xamarin.Forms.OpenTok.Service;
+using System.IO;
+
+namespace VideoCall.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = viewModel = new ItemsViewModel();
+
+            if (!CrossOpenTok.Current.TryStartSession())
+                return;
+
+            CrossOpenTok.Current.MessageReceived += OnMessageReceived;
+        }
+
+        async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            var item = args.SelectedItem as Item;
+            if (item == null)
+                return;
+
+            await Navigation.PushAsync(new ItemDetailPage(new ItemDetailViewModel(item)));
+
+            // Manually deselect item.
+            //ItemsListView.SelectedItem = null;
+        }
+
+        async void AddItem_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NavigationPage(new NewItemPage()));
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (viewModel.Items.Count == 0)
+                viewModel.LoadItemsCommand.Execute(null);
+        }
+
+        private void OnEndCall(object sender, EventArgs e)
+        {
+            CrossOpenTok.Current.EndSession();
+            CrossOpenTok.Current.MessageReceived -= OnMessageReceived;
+            Navigation.PopAsync();
+        }
+
+        private void OnMessage(object sender, EventArgs e)
+        {
+            CrossOpenTok.Current.SendMessageAsync("Hi!");
+        }
+
+        private void OnSwapCamera(object sender, EventArgs e)
+        {
+            CrossOpenTok.Current.CycleCamera();
+        }
+
+        private void OnMessageReceived(string message)
+        {
+            DisplayAlert("Message received", message, "OK");
+        }
+    }
+}
